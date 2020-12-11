e14e339feaf6c7573b98e0aa898b12c243b04bdf
minna123-cap <minura.samaranayake@gmail.com>
2020-12-11 14:12:10 +0530

    Update wsgi.py

--- elearningDash/elearningDash/wsgi.py
+++ elearningDash/elearningDash/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearningDash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearningDash.elearningDash.settings')
 
 application = get_wsgi_application()
